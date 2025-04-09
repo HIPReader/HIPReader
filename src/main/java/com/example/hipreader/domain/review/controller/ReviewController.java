8ea51f5fcf2781fde752a544560c148ec7419c8f
jeon-younghwan <younghwan314@naver.com>
2025-04-09 16:16:35 +0900

    fix: 응답 코드 수정

--- src/main/java/com/example/hipreader/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/hipreader/domain/review/controller/ReviewController.java
@@ -8,6 +8,7 @@ import com.example.hipreader.domain.review.dto.response.ReviewUpdateResponseDto;
 import com.example.hipreader.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +23,12 @@ public class ReviewController {
 
     @PostMapping("/reviews")
     public ResponseEntity<ReviewCreateResponseDto> createReview(@RequestBody @Valid ReviewCreateRequestDto requestDto) {
-        return ResponseEntity.ok(reviewService.createReview(requestDto));
+        return new ResponseEntity<>(reviewService.createReview(requestDto), HttpStatus.CREATED);
     }
 
     @GetMapping("/books/{bookId}/reviews")
     public ResponseEntity<List<ReviewReadResponseDto>> getReviews(@PathVariable Long bookId) {
-        return ResponseEntity.ok(reviewService.getReviews(bookId));
+        return new ResponseEntity<>(reviewService.getReviews(bookId), HttpStatus.OK);
     }
 
     @GetMapping("/books/{bookId}/reviews/{reviewId}")
@@ -35,12 +36,12 @@ public class ReviewController {
             @PathVariable Long bookId,
             @PathVariable Long reviewId
     ) {
-        return ResponseEntity.ok(reviewService.getReview(bookId, reviewId));
+        return new ResponseEntity<>(reviewService.getReview(bookId, reviewId), HttpStatus.OK);
     }
 
     @PatchMapping("/books/{bookId}/reviews/{reviewId}")
     public ResponseEntity<ReviewUpdateResponseDto> updateReview(@PathVariable Long bookId, @PathVariable Long reviewId, @RequestBody ReviewUpdateRequestDto requestDto) {
-        return ResponseEntity.ok(reviewService.updateReview(bookId, reviewId, requestDto));
+        return new ResponseEntity<>(reviewService.updateReview(bookId, reviewId, requestDto), HttpStatus.OK);
     }
 
     @DeleteMapping("/books/{bookId}/reviews/{reviewId}")
