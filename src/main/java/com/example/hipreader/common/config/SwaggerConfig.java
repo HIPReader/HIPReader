959ab7265621110cfd3150263145f9d37899b3db
KimKiHong-1111 <rlghd0157@gmail.com>
2025-04-17 10:24:17 +0900

    "feat: swagger 설정"

--- src/main/java/com/example/hipreader/common/config/SwaggerConfig.java
+++ src/main/java/com/example/hipreader/common/config/SwaggerConfig.java
@@ -5,15 +5,29 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 	@Bean
-	public OpenAPI openAPI() {
+	public OpenAPI customOpenAPI() {
+		// Security Scheme 정의
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name("Authorization");
+
+		// Security Requirement 정의
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
 		return new OpenAPI()
-			.info(new Info()
-				.title("API 문서 제목")
-				.description("API 설명")
-				.version("1.0.0"));
+			.info(new Info().title("API 문서")
+				.description("API 명세서")
+				.version("v1.0"))
+			.addSecurityItem(securityRequirement) // Security Requirement 추가
+			.schemaRequirement("BearerAuth", securityScheme); // Security Scheme 추가
 	}
 }
