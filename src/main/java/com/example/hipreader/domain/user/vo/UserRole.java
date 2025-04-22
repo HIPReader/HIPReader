f9dfe9cfc3a48502fa74dbea9ffd905236eac22f
KimKiHong-1111 <rlghd0157@gmail.com>
2025-04-22 10:30:35 +0900

    "refactor : user 리팩토링"

--- src/main/java/com/example/hipreader/domain/user/vo/UserRole.java
+++ src/main/java/com/example/hipreader/domain/user/vo/UserRole.java
@@ -1,11 +1,14 @@
 package com.example.hipreader.domain.user.vo;
 
+import static com.example.hipreader.common.exception.ErrorCode.*;
+
 import java.util.Arrays;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.hipreader.common.exception.ErrorCode;
+import com.example.hipreader.common.exception.NotFoundException;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +26,7 @@ public enum UserRole implements GrantedAuthority {
 		return Arrays.stream(UserRole.values())
 			.filter(r -> r.name().equalsIgnoreCase(role))
 			.findFirst()
-			.orElseThrow(() -> new ResponseStatusException(ErrorCode.INVALID_USER_ROLE.getStatus()));
+			.orElseThrow(() -> new NotFoundException(INVALID_USER_ROLE));
 	}
 
 	@Override
