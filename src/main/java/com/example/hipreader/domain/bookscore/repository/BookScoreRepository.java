9b110e46ba67818a92a56e3d69b6a05d796f7dc8
KimKiHong-1111 <rlghd0157@gmail.com>
2025-04-28 11:02:25 +0900

    "refactor: n+1문제 해결 "

--- src/main/java/com/example/hipreader/domain/bookscore/repository/BookScoreRepository.java
+++ src/main/java/com/example/hipreader/domain/bookscore/repository/BookScoreRepository.java
@@ -2,6 +2,7 @@ package com.example.hipreader.domain.bookscore.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,6 +10,6 @@ import com.example.hipreader.domain.book.entity.Book;
 import com.example.hipreader.domain.bookscore.entity.BookScore;
 
 public interface BookScoreRepository extends JpaRepository<BookScore, Long> {
-	@Query("SELECT bs FROM BookScore bs WHERE bs.book = :book")
+	@EntityGraph(attributePaths = "book.authors")
 	Optional<BookScore> findByBook(Book book);
 }
